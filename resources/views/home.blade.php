83008aef7d8562ef5ef8824a83c2685863c0e0c4
Jonathan Pillajo <pandc19@outlook.com>
2024-09-04 05:38:52 -0500

    end section 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Welcome!') }}</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,8 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('This is a payment processing system, using the most popular
-                        payment platforms available') }}
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
