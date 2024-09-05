e7f5d74ce9b8d8d7cda49805b4f463b6118be544
Jonathan Pillajo <pandc19@outlook.com>
2024-09-05 06:58:12 -0500

    end section 6

--- config/services.php
+++ config/services.php
@@ -14,6 +14,12 @@ return [
     |
     */
 
+    'paypal' => [
+        'base_uri' => env('PAYPAL_BASE_URI'),
+        'client_id' => env('PAYPAL_CLIENT_ID'),
+        'client_secret' => env('PAYPAL_CLIENT_SECRET'),
+    ],
+
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
